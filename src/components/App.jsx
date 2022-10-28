71fedff7366983d4087239d444cc13d3d03beb2d
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-10-28 18:41:19 +0100

    change dependens

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import { Section } from './Section/Section';
 import { ContactList } from './PhoneList/PhoneList';
 import { Filter } from './Filter/Filter';
 import { useEffect } from 'react';
-import {fetchContacts} from 'redux/contactsOperations';
+import { fetchContacts } from 'redux/contactsOperations';
 import { useDispatch } from 'react-redux';
 
 export const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchContacts());
-  }, []);
+  }, [dispatch]);
   return (
     <>
       <Section title="PhoneContact">
